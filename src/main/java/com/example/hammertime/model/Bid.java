15928661bd2eac127a01cfdc6e84309b0f51b202
hibany1 <akobelev741@gmail.com>
2026-05-27 20:04:55 +0400

    Добавил Bid model

---
+++ src/main/java/com/example/hammertime/model/Bid.java
@@ -0,0 +1,61 @@
+package com.example.hammertime.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Bid {
+    private Long id;
+    private Long lotId;
+    private Long userId;
+    private String userName;
+    private BigDecimal amount;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getLotId() {
+        return lotId;
+    }
+
+    public void setLotId(Long lotId) {
+        this.lotId = lotId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
