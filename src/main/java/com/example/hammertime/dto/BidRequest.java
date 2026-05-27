49ea409ad5a4d48dd1928786826b39db8983831f
hibany1 <akobelev741@gmail.com>
2026-05-27 20:08:35 +0400

    Добавил DTO запроса ставки

---
+++ src/main/java/com/example/hammertime/dto/BidRequest.java
@@ -0,0 +1,15 @@
+package com.example.hammertime.dto;
+
+import java.math.BigDecimal;
+
+public class BidRequest {
+    private BigDecimal amount;
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
